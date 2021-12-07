5996ad4624130601bd113979baa25350abfb526e
= <romain.bzin@gmail.com>
2021-12-07 12:04:44 +0100

    apojglajgp

--- Sources/headers/Player.h
+++ Sources/headers/Player.h
@@ -1,7 +1,7 @@
 #pragma once
-#include "SFML-2.5.1/include/SFML/Graphics.hpp"
-#include "SFML-2.5.1/include/SFML/System.hpp"
-#include "SFML-2.5.1/include/SFML/Window.hpp"
+#include "../../SFML-2.5.1/include/SFML/Graphics.hpp"
+#include "../../SFML-2.5.1/include/SFML/System.hpp"
+#include "../../SFML-2.5.1/include/SFML/Window.hpp"
 #include <vector>
 
 class Player
